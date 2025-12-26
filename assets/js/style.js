af87a909a4892a92a0c6f88826c0ba00d2a0736b
giftin.id <calistanafisaa@gmail.com>
2025-12-26 13:03:05 +0700

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I LOVE YOU, OWES').split('');
+        const titles = ('I LOVE YOU').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "I love you forevermore, Owes";
+            subtitle.innerText = "Thank you for staying, for trying, for choosing us. I hope we keep growing, keep understanding each other, and keep finding reasons to stay close";
             subtitle.style.fontSize = '20px'; 
             subtitle.style.marginTop = '2px'; 
             subtitle.style.color = '#fff';
